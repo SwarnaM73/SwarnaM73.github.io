e8d0cd46c76510e7fa75b75e7bdc962415f1c9e9
SwarnaM73 <75279314+SwarnaM73@users.noreply.github.com>
2020-11-30 16:52:08 -0600

    Update wdc.js

--- wdc.js
+++ wdc.js
@@ -1,5 +1,6 @@
 (function () {
 var myConnector = tableau.makeConnector();
+  
 myConnector.getSchema = function (schemaCallback) {
 var cols = [
 { id : "@timestamp", alias:"@timestamp", dataType : tableau.dataTypeEnum.datetime },
@@ -13,6 +14,7 @@ columns : cols
 };
 schemaCallback([tableInfo]);
 };
+  
 myConnector.getData = function(table, doneCallback) {
 $.getJSON("https://cl1-vmcrpees-01.multiplan.com:9200/*appdynamics-snapshots*/_search", function(resp) {
 var feat = resp;
@@ -21,14 +23,16 @@ tableData = [];
 for (var i = 0, len = feat.length; i < len; i++) {
 tableData.push({
 "@timestamp": feat[i].@timestamp,
-"name": feat[i].businesstransaction.name,
+"name": feat[i].businesstransaction.name
 });
 }
 table.appendRows(tableData);
 doneCallback();
 });
 };
+  
 tableau.registerConnector(myConnector);
+  
 $(document).ready(function () {
 $("#submitButton").click(function () {
 tableau.connectionName = "AppData";
