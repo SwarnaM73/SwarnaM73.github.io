d7999f134bc4b01861af2374559007c03d8b5201
SwarnaM73 <75279314+SwarnaM73@users.noreply.github.com>
2020-12-10 15:43:40 -0600

    Update wdc.js

--- wdc.js
+++ wdc.js
@@ -16,7 +16,7 @@ schemaCallback([tableInfo]);
 };
   
 myConnector.getData = function(table, doneCallback) {
-$.getJSON("https://cl1-vmcrpees-01.multiplan.com:9200/*appdynamics-snapshots*/_search", crossorigin="anonymous", function(resp) {
+$.getJSON("https://cl1-vmcrpees-01.multiplan.com:9200/*appdynamics-snapshots*/_search", function(resp) {
 var feat = resp;
 tableData = [];
 for (var i = 0, len = feat.length; i < len; i++) {
